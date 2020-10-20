512195ab714b23471e61533be6e895d9cbaf9a91
Kevin Pilch <me@pilchie.com>
2020-10-20 20:53:06 +0000

    Add project and launch assets

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace devcontainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
